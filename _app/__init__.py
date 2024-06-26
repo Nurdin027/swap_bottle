fe17aec91e07a31e5765a6651691c65e286238ae
Nurdin027 <amarnurdin000@gmail.com>
2024-06-26 11:48:02 +0700

    random shuffle

--- _app/__init__.py
+++ _app/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,13 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    import random
-    pil, jaw = [], []
-    while len(pil) < 6 or len(jaw) < 6:
-        p = random.randint(1, 6)
-        j = random.randint(1, 6)
-        if p not in pil and len(pil) < 6:
-            pil.append(p)
-        if j not in jaw and len(jaw) < 6:
-            jaw.append(j)
+    pil, jaw = [*range(1, 7)], [*range(1, 7)]
+    random.shuffle(pil)
+    random.shuffle(jaw)
     return render_template('main.html', pil=pil, jaw=jaw)
